8335bcd55eb4ac32258d7bec274732f06f96743b
Atul Singh <91019514+atulrmsingh@users.noreply.github.com>
2022-05-11 10:36:29 +0530

    Update NumberGuessingGame.java

--- src/NumberGuessingGame.java
+++ src/NumberGuessingGame.java
@@ -3,15 +3,15 @@ import java.util.Scanner;
 public class NumberGuessingGame {
 	public static void main(String[] args) {
 		int max = 100, min = 0, mid = min + max / 2;
-		Scanner sc = new Scanner(System.in);
+		Scanner userInput = new Scanner(System.in);
 		System.out.println("Guess a number between 1 to 100");
 		while (true) {
 			System.out.println("Press 1 if your number is greater than or equal to: " + mid);
 			System.out.println("Press 2 if your number is less than : " + mid);
-			int input = sc.nextInt();
+			int input = userInput.nextInt();
 			if (input == 1) {
 				System.out.println("Is your number is : " + mid + " Press 1");
-				int num = sc.nextInt();
+				int num = userInput.nextInt();
 				if (num == 1) {
 					break;
 				} else {
